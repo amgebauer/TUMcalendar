44362cb79226c297c305af0c52ea2a2a38f4a916
Amadeus Gebauer <amadeus.gebauer@gmail.com>
2017-04-23 22:02:29 +0200

    Moved project to root and removed admin panel

--- TUMcalendar/urls.py
+++ TUMcalendar/urls.py
@@ -17,6 +17,5 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^TUMcalendar/', include('TUMCalendarTools.urls')),
+    url(r'^$', include('TUMCalendarTools.urls')),
 ]

d3f33425643420872bf64fc039c0765ef89327a3
Amadeus Gebauer <amadeus.gebauer@gmail.com>
2017-04-23 22:10:10 +0200

    Fixed bug with root directory

--- TUMcalendar/urls.py
+++ TUMcalendar/urls.py
@@ -17,5 +17,5 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^$', include('TUMCalendarTools.urls')),
+    url(r'^', include('TUMCalendarTools.urls')),
 ]
